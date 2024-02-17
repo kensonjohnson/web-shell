345dc30f37b6723602fe217b38708111d6b9fc75
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-16 20:57:58 -0600

    Extract constants to seperate file

---
+++ frontend/constants.ts
@@ -0,0 +1,56 @@
+export type CommandOptionsMap = {
+  [command: string]: Command;
+};
+
+export type Command = {
+  acceptsArguments: boolean;
+  requiresArguments: boolean;
+  options: Record<string, Option>;
+};
+
+export type Option = {
+  acceptsArguments: boolean;
+  description: string;
+};
+
+export const COMMAND_OPTIONS_MAP: CommandOptionsMap = {
+  ls: {
+    acceptsArguments: true,
+    requiresArguments: false,
+    options: {
+      l: { acceptsArguments: false, description: "Detailed list" },
+      a: { acceptsArguments: false, description: "Show hidden files" },
+      p: {
+        acceptsArguments: false,
+        description: "Add / to end of directories",
+      },
+    },
+  },
+  cat: {
+    acceptsArguments: true,
+    requiresArguments: true,
+    options: {
+      n: { acceptsArguments: false, description: "Add number lines" },
+      s: { acceptsArguments: false, description: "Remove extra spaces" },
+    },
+  },
+  ps: {
+    acceptsArguments: false,
+    requiresArguments: false,
+    options: {
+      e: {
+        acceptsArguments: false,
+        description: "Show processes from all users",
+      },
+      x: {
+        acceptsArguments: false,
+        description: "Show processes without a controlling terminal",
+      },
+      c: { acceptsArguments: false, description: "Show the command only" },
+      f: {
+        acceptsArguments: false,
+        description: "Show full (verbose) listing",
+      },
+    },
+  },
+};
